fa2ebd63352e7b45d60d327271a34c1fa450a00e
olashipsjs <olanrewajuoladejo08@gmail.com>
2025-01-20 20:21:27 +0000

    📝 Update App component content and remove dark mode styles in index.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum corporis
+        dolorum ad? Saepe voluptatem architecto rerum, facere dolorem accusamus
+        inventore earum quos, maxime minus facilis voluptatibus nesciunt ipsa,
+        natus beatae?
+      </p>
+    </div>
+  );
 };
 
 export default App;
