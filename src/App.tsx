846a214dd9c8bb7ee283ef273a7512901e135147
olashipsjs <olanrewajuoladejo08@gmail.com>
2025-01-20 20:32:04 +0000

    Jan 20, 2025, 9:32 PM

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
-    <div className='min-h-screen flex flex-col items-end justify-center bg-purple-400 max-w-[400px] mx-auto p-4'>
-      <div className='bg-slate-700'>
+    <div className='min-h-screen flex flex-col items-center justify-end max-w-[400px] mx-auto p-4'>
+      <div className='bg-gray-900 w-4/5 rounded-full'>
         <h6 className='underline'>Introduction</h6>
       </div>
     </div>
