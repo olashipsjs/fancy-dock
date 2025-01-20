7bfba86756bcc73d623e843d3c155cc9896519b2
olashipsjs <olanrewajuoladejo08@gmail.com>
2025-01-20 20:38:28 +0000

    ✨ Enhance introduction section with additional styling and a progress button

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 const App = () => {
   return (
     <div className='min-h-screen flex flex-col items-center justify-end max-w-[400px] mx-auto p-4'>
-      <div className='bg-gray-900 w-4/5 rounded-full'>
-        <h6 className='underline'>Introduction</h6>
+      <div className='bg-gray-900 w-4/5 rounded-full flex overflow-clip items-center p-1'>
+        <h6 className='underline text-white text-sm font-medium flex-1'>
+          Introduction
+        </h6>
+        <button className='py-1.5 px-3 rounded-full text-white leading-[1] bg-gray-500 text-xs font-semibold'>
+          10%
+        </button>
       </div>
     </div>
   );
