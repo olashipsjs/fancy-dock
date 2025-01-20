8de397b3d6c5dca6b2834ec51c4968314260fb6e
olashipsjs <olanrewajuoladejo08@gmail.com>
2025-01-20 20:56:32 +0000

    📚 Update UI to display a list of titles and improve button styling and layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,46 @@
 import { ArrowLeft } from './components/icons/ArrowLeft';
 
+const titles = [
+  'Intro',
+  'Overview',
+  'Identifying the problem',
+  'UI & rebrand',
+  'A stack comparison',
+  'Conclusion',
+];
+
 const App = () => {
   return (
     <div className='min-h-screen flex flex-col items-center justify-end max-w-[400px] mx-auto p-4'>
-      <div className='bg-gray-900 w-4/5 rounded-full flex overflow-clip items-center py-1 pr-1 pl-2'>
-        <button className='size-5 bg-transparent hover:bg-gray-500 flex items-center justify-center rounded-full'>
-          <ArrowLeft
-            width={16}
-            height={16}
-            className={'text-white'}
-          />
-        </button>
-        <h6 className='underline text-white text-sm font-medium flex-1'>
-          Introduction
-        </h6>
-        <button className='py-1.5 px-3 rounded-full text-white leading-[1] bg-gray-500 text-xs font-semibold'>
-          10%
-        </button>
+      <div className='bg-gray-900 w-4/5 rounded-xl flex flex-col gap-4 overflow-clip p-1'>
+        <div className='flex overflow-x-auto flex-col'>
+          {titles.map((title, index) => {
+            return (
+              <button
+                key={index}
+                className={`text-white w-full flex flex-col text-sm font-medium py-1.5 px-2 transition-all duration-200 ease-linear cursor-pointer hover:bg-gray-500 rounded-lg ${
+                  index !== 0 && 'text-gray-500 hover:text-white'
+                }`}
+              >
+                {title}
+              </button>
+            );
+          })}
+        </div>
+
+        <div className='flex items-center gap-2'>
+          <button className='size-6 bg-transparent hover:bg-gray-500 flex items-center justify-center rounded-full'>
+            <ArrowLeft
+              width={16}
+              height={16}
+              className={'text-white'}
+            />
+          </button>
+          <h6 className='text-white text-sm font-medium flex-1'>Intro</h6>
+          <button className='py-1.5 px-3 rounded-full text-white leading-[1] bg-gray-500 text-xs font-semibold'>
+            10%
+          </button>
+        </div>
       </div>
     </div>
   );
