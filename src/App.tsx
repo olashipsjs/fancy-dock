ae6d158c36712930b54aab18be64cf08588ab1c2
olashipsjs <olanrewajuoladejo08@gmail.com>
2025-01-20 21:51:19 +0000

    🧹 Remove redundant max-width class from container div in App component

--- src/App.tsx
+++ src/App.tsx
@@ -38,7 +38,7 @@ const App = () => {
     <div className='min-h-screen flex flex-col items-center justify-end max-w-[400px] mx-auto p-4'>
       <div
         id='container'
-        className='bg-gray-900 rounded-[24px] transition-all duration-200 ease-out flex flex-col overflow-clip p-1 max-w-full mx-auto'
+        className='bg-gray-900 rounded-[24px] transition-all duration-200 ease-out flex flex-col overflow-clip p-1'
       >
         <div
           id='titles'
