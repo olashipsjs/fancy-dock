b7eb2ebc3518e67166127c63632361d313262a7f
olashipsjs <olanrewajuoladejo08@gmail.com>
2025-01-20 20:44:26 +0000

    🔘 Add button element around ArrowLeft icon with hover effect in App component

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { ArrowLeft } from './components/icons/ArrowLeft';
 const App = () => {
   return (
     <div className='min-h-screen flex flex-col items-center justify-end max-w-[400px] mx-auto p-4'>
-      <div className='bg-gray-900 w-4/5 rounded-full flex overflow-clip items-center py-1 px-2'>
-        <ArrowLeft
-          width={16}
-          height={16}
-          className={'text-white'}
-        />
+      <div className='bg-gray-900 w-4/5 rounded-full flex overflow-clip items-center py-1 pr-1 pl-2'>
+        <button className='size-5 bg-transparent hover:bg-gray-500 flex items-center justify-center rounded-full'>
+          <ArrowLeft
+            width={16}
+            height={16}
+            className={'text-white'}
+          />
+        </button>
         <h6 className='underline text-white text-sm font-medium flex-1'>
           Introduction
         </h6>
