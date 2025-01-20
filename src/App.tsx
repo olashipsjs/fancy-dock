5c3923d43a747a7da54de91ebfddd46cc167be93
olashipsjs <olanrewajuoladejo08@gmail.com>
2025-01-20 20:28:09 +0000

    ✨ Update App component styling and remove redundant CSS rules

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 const App = () => {
   return (
-    <div className='min-h-screen flex flex-col items-end justify-center bg-purple-400'>
+    <div className='min-h-screen flex flex-col items-end justify-center bg-purple-400 max-w-[400px] mx-auto p-4'>
       <div className='bg-slate-700'>
-        <h6>Introduction</h6>
+        <h6 className='underline'>Introduction</h6>
       </div>
     </div>
   );
